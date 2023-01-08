0fe7b273f60cbe78a73c48a5b867b6446ba5b863
Razorphyn <est.grandi@gmail.com>
2023-01-08 18:36:05 +0100

    Version Update

--- mangaerordini/Properties/AssemblyInfo.cs
+++ mangaerordini/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Numero di build
 //      Revisione
 //
-[assembly: AssemblyVersion("4.0.0.1")]
-[assembly: AssemblyFileVersion("4.0.0.1")]
+[assembly: AssemblyVersion("4.0.0.2")]
+[assembly: AssemblyFileVersion("4.0.0.2")]
